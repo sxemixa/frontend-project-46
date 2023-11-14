7879910213cd9a80026bbc06c79c185904efc078
sxemixa <sxemixa@gmail.com>
2023-11-14 23:23:33 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,9 @@
 import { program } from 'commander';
 
 program.version('0.0.1', '-V, --version', 'output the version number');
+program.argument('<filepath1>');
+program.argument('<filepath2>');
 program.description('Compares two configuration files and shows a difference.')
-program.helpOption('-h, --help', 'output usage information');
-
+program.helpOption('-h, --help', 'output usage information')
+program.option('-f, --format <type>',  'output format');
 program.parse();
