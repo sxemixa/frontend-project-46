72cc855edaff6a8a43f3dfc7fae67ceb3f1368b2
sxemixa <sxemixa@gmail.com>
2023-11-18 00:07:47 +0300

    step2 v.3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import * as fs from 'node:fs'; 
+import { getProcessedFile } from '../src/index.js';
 
 program.version('0.0.1', '-V, --version', 'output the version number');
 program.argument('<filepath1>');
@@ -10,7 +10,7 @@ program.description('Compares two configuration files and shows a difference.')
 program.helpOption('-h, --help', 'output usage information')
 program.option('-f, --format <type>',  'output format')
 program.action((filepath1, filepath2) => {
-    console.log(fs.readFileSync('file1.json', 'utf8'));
-    console.log(fs.readFileSync('file2.json', 'utf8'));
+    console.log(getProcessedFile('file1.json'));
+    console.log(getProcessedFile('file2.json'));
         });
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-//import path from 'node:path';
-//import * as fs from 'node:fs'; 
+import path from 'node:path';
+import fs from 'node:fs'; 
 
-//const getAbsolutePath = (filepath) = path.resolve(process.cwd(), filepath);
-//const readFile = (filepath) = fs.readFileSync(getAbsolutePath(filepath));
+const getAbsolutePath = (filepath) => {
+    return path.resolve(process.cwd(), filepath);
+   };
+const readFile = (filepath) => {
+    return fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+};
+const parse = (filepath) => {
+    return JSON.parse(filepath);
+};
+
+const getProcessedFile = (filepath) => {
+    const result = parse(readFile(getAbsolutePath(filepath)));
+    return result;
+};
+
+export { getProcessedFile };
+
+//объединение ключей
+//что такое технический долг?
+//const keys = _.union(_.keys(obj1), _keys(obj2));
+
+
+
+
+
+
+
+
+//const build = (key, obj1, obj2) => {
+   // const val1 = obj1[key];
+   // const val2 = obj2[key];
+   // if (val1 && val2) {
+       // if (val1 === val2 || (_.isObject(val1) && _.isObject(val2))) {
+            //const children = _.isObject(val2) ? buildDiff(val1, val2) : [];
+            //return { key, childern, type: 'unchanged',
+                //value: _.isObject(val1) ? null : val1,
+         //   };
+      //  }
+   // }
+//};
