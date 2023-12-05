edd1c130b6430ff5f78993d2b1d56d7f11693240
sxemixa <sxemixa@gmail.com>
2023-12-05 23:41:35 +0300

    fix reviewer marks

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,7 +5,7 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync((getFixturePath(filename)), 'utf-8');
 
 describe('genDiff', () => {
@@ -13,12 +13,14 @@ describe('genDiff', () => {
   const expectPlain = readFile('expectedPlain.txt');
   const expectJson = readFile('expectedJson.txt');
 
-  test.each(['json', 'yaml'])('formats', (format) => {
+  describe.each(['json', 'yaml'])('formats', (format) => {
     const filepath1 = getFixturePath(`file1.${format}`);
     const filepath2 = getFixturePath(`file2.${format}`);
-    expect(genDiff(filepath1, filepath2)).toBe(expectStylish);
-    expect(genDiff(filepath1, filepath2, 'stylish')).toBe(expectStylish);
-    expect(genDiff(filepath1, filepath2, 'plain')).toBe(expectPlain);
-    expect(genDiff(filepath1, filepath2, 'json')).toBe(expectJson);
+    test.each([
+      ['stylish', expectStylish],
+      ['plain', expectPlain],
+      ['json', expectJson]])('formatters: %s', (formatter, expected) => {
+      expect(genDiff(filepath1, filepath2, formatter)).toBe(expected);
+    });
   });
 });
